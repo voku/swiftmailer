eb7d3b8656dabb0e93b8e52b2bdc5155b5cc88ed
Lars Moelleken <lmoelleken@menadwork.com>
2015-09-19 01:11:08 +0200

    [*}: only code-style

--- lib/classes/Swift/SmtpTransport.php
+++ lib/classes/Swift/SmtpTransport.php
@@ -33,9 +33,8 @@ class Swift_SmtpTransport extends Swift_Transport_EsmtpTransport
     {
         call_user_func_array(
             array($this, 'Swift_Transport_EsmtpTransport::__construct'),
-            Swift_DependencyContainer::getInstance()
-                ->createDependenciesFor('transport.smtp')
-            );
+            Swift_DependencyContainer::getInstance()->createDependenciesFor('transport.smtp')
+        );
 
         $this->setHost($host);
         $this->setPort($port);
