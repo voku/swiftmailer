b59de0b396137c093a64b6550765077260a4c35e
Lars Moelleken <lmoelleken@menadwork.com>
2015-09-21 09:38:46 +0200

    [+]: re-add "Swift_CharacterStream_NgCharacterStream"

--- lib/dependency_maps/mime_deps.php
+++ lib/dependency_maps/mime_deps.php
@@ -71,7 +71,7 @@ Swift_DependencyContainer::getInstance()
     ->withDependencies(array('mime.charstream'))
 
     ->register('mime.charstream')
-    ->asNewInstanceOf('Swift_CharacterStream_MbCharacterStream')
+    ->asNewInstanceOf('Swift_CharacterStream_NgCharacterStream')
     ->withDependencies(array('mime.characterreaderfactory', 'properties.charset'))
 
     ->register('mime.bytecanonicalizer')
