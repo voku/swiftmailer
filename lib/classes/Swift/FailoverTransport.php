4339ffebd991c65246e62e0b093d09ec0be2e0a2
Lars Moelleken <lars@moelleken.org>
2016-04-13 10:59:38 +0200

    [+] try to fix Travis CI build
    "Ruby 1.9 no longer supported due to Mailcatcher's dependencies?"
    
    -> https://github.com/sj26/mailcatcher/issues/277

--- lib/classes/Swift/FailoverTransport.php
+++ lib/classes/Swift/FailoverTransport.php
@@ -24,9 +24,8 @@ class Swift_FailoverTransport extends Swift_Transport_FailoverTransport
     {
         call_user_func_array(
             array($this, 'Swift_Transport_FailoverTransport::__construct'),
-            Swift_DependencyContainer::getInstance()
-                ->createDependenciesFor('transport.failover')
-            );
+            Swift_DependencyContainer::getInstance()->createDependenciesFor('transport.failover')
+        );
 
         $this->setTransports($transports);
     }
